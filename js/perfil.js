64b727a1afc9de0e03ad8f28e660a694e21c24ef
karpalospo <ing.cmfernandez@hotmail.com>
2023-05-26 08:51:28 -0500

    mayo 26

--- js/perfil.js
+++ js/perfil.js
@@ -1,15 +1,16 @@
 async function page_init() {
 
     //if(!store.user.logged) parent.location = "index.html"
-    
+    let d;
 
     res = await API.POST.userProfile(store.user.nit, store.user.nombres, store.user.email, store.user.auth_token)
     if(!res.error) {
-        // $("#frm-user-email").val(store.user.email)
-        // $("#frm-user-nombres").val(store.user.nombres)
-        // $("#frm-user-cc").val(store.user.nit)
-        // $("#frm-user-celular").val(res.data.celular)
-        // $("#frm-user-nacimiento").val(res.data.fecha_nacimiento)
+        d = res.data
+        $("#frm-user-email").val(d.email)
+        $("#frm-user-nombres").val(d.nombres)
+        $("#frm-user-cc").val(d.nit)
+        $("#frm-user-celular").val(d.celular)
+        $("#frm-user-nacimiento").val(d.fecha_nacimiento)
     }
     
 }
