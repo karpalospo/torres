2ab0e4c54d0014c7af1be624703375ba1c4c2149
Jorge Ramos Martinez <jorge.erm@hotmail.com>
2023-06-01 12:52:05 -0500

    Torres Server

---
+++ routes/home/route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+router.use(function timeLog(req, res, next) {
+  next();
+});
+router.get('/', function(req, res) {    
+    res.sendFile(path.join(global.raiz, './views', 'index.html'));
+});
+
+module.exports = router;
