c9be16c3497123f2822fc9f8cff3f0f2fe8c822f
karpalospo <ing.cmfernandez@hotmail.com>
2023-08-14 15:54:07 -0500

    tag manager

--- src/opt/site/views/global.js
+++ src/opt/site/views/global.js
@@ -10,6 +10,9 @@ return /*html*/`
 <!DOCTYPE html>
 <html lang="es">
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5NLTF7C2');</script>
+	<!-- End Google Tag Manager -->
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<title>Farmacia Torres | ${param.title}</title>
@@ -18,8 +21,13 @@ return /*html*/`
 	${param.userCSS ? `<link href="${ABS_URL}/css/user.css" rel="stylesheet"></link>` : ""}
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
 	<link rel="shortcut icon" href="${ABS_URL}/assets/icon_logo.png" />
+
 </head>
-<body>`},
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NLTF7C2" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+`},
 
 	userMenu: /*html*/`
 <div id="user-menu-cont">
