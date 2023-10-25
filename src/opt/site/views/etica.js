36d080677e44610234491b0cf2adf42099236628
Karpalospo <ing.cmfernandez@hotmail.com>
2023-10-25 11:18:50 -0500

    char

--- src/opt/site/views/etica.js
+++ src/opt/site/views/etica.js
@@ -9,7 +9,8 @@ ${HTML.header}
 <div id="content" style="background-color: transparent;">
 	<div class="bg-blanco" style="text-align: justify; margin: 80px auto">
 		<br>
-		<h2>Programa de Transparencia y Ética Empresarial</h2>
+		<h2>Sistema de Autocontrol y Gestión del Riesgo Integral de Lavado de Activos, Financiación del Terrorismo y Financiamiento de la Proliferación de Armas de Destrucción Masiva – SAGRILAFT
+Programa de Transparencia y Ética Empresarial PTEE</h2>
 		<p class="mt-3">Eticos Ltda cuenta con un Sistema de Autocontrol y Gestión del Riesgo Integral de Lavado de Activos, Financiación del Terrorismo y Financiamiento de la Proliferación de Armas de Destrucción Masiva  - SAGRILAFT y un Programa de Transparencia y Ética Empresarial PTEE.</p>
 		<p class="mt-3">Si llegase a tener conocimiento de alguna actividad ilegal o irregular por parte de nuestros colaboradores, empleados, proveedores, asociados o clientes. Puede notificar al Oficial de Cumplimiento de ETICOS LTDA a través del correo electrónico <b>cumplimiento@eticos.com</b></p>
 		<br>
